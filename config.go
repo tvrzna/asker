fd45764091a5f25f3037d57305c40b81a38e9b20
Michal Tvrznik <emporeor@gmail.com>
2021-09-07 10:29:23 +0200

    Left using flags and used sufficient custom implementation.

--- config.go
+++ config.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type enMsgType int
@@ -22,69 +22,70 @@ type config struct {
 }
 
 func loadConfig() *config {
-	conf := &config{}
+	conf := &config{msgType: warning, title: "asker", message: "Please see help how to use this application."}
 
-	var showConfirm bool
-	var showWarning bool
-	var showInfo bool
-	var showPassword bool
-	var title string
-	var message string
-
-	var printHelp bool
-	var printVersion bool
-
-	flag.BoolVar(&printHelp, "h", false, "print this help")
-
-	flag.BoolVar(&printVersion, "v", false, "print version")
-
-	flag.StringVar(&title, "t", "asker", "")
-
-	flag.StringVar(&message, "m", "", "")
-
-	flag.BoolVar(&showConfirm, "c", false, "")
-
-	flag.BoolVar(&showWarning, "w", false, "")
-
-	flag.BoolVar(&showInfo, "i", false, "")
-	flag.BoolVar(&showInfo, "-info", false, "")
-
-	flag.BoolVar(&showPassword, "p", false, "")
-
-	flag.Parse()
-
-	if printHelp {
-		fmt.Println("Usage: asker [options]")
-		fmt.Println("Options:")
-		fmt.Printf("  -h\t\tprint this help\n")
-		fmt.Printf("  -v\t\tprint version\n")
-		fmt.Printf("  -t TEXT\t\tsets the title\n")
-		fmt.Printf("  -m TEXT\t\tsets the message\n")
-		fmt.Printf("  -c\t\tshows confirm dialog \n")
-		fmt.Printf("  -i\t\tshows info dialog \n")
-		fmt.Printf("  -w,\t\tshows warning dialog \n")
-		fmt.Printf("  -p\t\tshows password dialog \n")
-
+	if contains(os.Args, "-h") || contains(os.Args, "--help") {
+		printHelp()
 		os.Exit(0)
 	}
-
-	if printVersion {
+	if contains(os.Args, "-v") || contains(os.Args, "--version") {
 		fmt.Println(getVersion())
 		os.Exit(0)
 	}
 
-	if showPassword {
-		conf.msgType = password
-	} else if showWarning {
-		conf.msgType = warning
-	} else if showInfo {
-		conf.msgType = info
-	} else {
-		conf.msgType = confirm
+	for i, arg := range os.Args {
+		switch arg {
+		case "-t", "--title":
+			nextArg(os.Args, i, func(val string) {
+				conf.title = strings.TrimSpace(val)
+			})
+		case "-m", "--message":
+			nextArg(os.Args, i, func(val string) {
+				conf.message = strings.TrimSpace(val)
+			})
+		case "-c", "--confirm":
+			conf.msgType = confirm
+		case "-w", "--warning":
+			conf.msgType = warning
+		case "-i", "--info":
+			conf.msgType = info
+		case "-p", "--pasword":
+			conf.msgType = password
+		}
 	}
 
-	conf.title = title
-	conf.message = message
-
 	return conf
+}
+
+// Checks, if array contains value
+func contains(array []string, value string) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+// Gets next argument, if available
+func nextArg(args []string, i int, callback func(value string)) {
+	if callback != nil && len(args) > i+1 {
+		val := strings.TrimSpace(args[i+1])
+		if !strings.HasPrefix(val, "-") {
+			callback(args[i+1])
+		}
+	}
+}
+
+func printHelp() {
+	fmt.Println("Usage: asker [options]")
+	fmt.Println("Options:")
+	fmt.Printf("  -h, --help\t\t\tprint this help\n")
+	fmt.Printf("  -v, --version\t\t\tprint version\n")
+	fmt.Printf("  -t TEXT, --title TEXT\t\tsets the title\n")
+	fmt.Printf("  -m TEXT, --message TEXT\tsets the message\n")
+	fmt.Printf("  -c, --confirm\t\t\tshows confirm dialog \n")
+	fmt.Printf("  -i, --info\t\t\tshows info dialog \n")
+	fmt.Printf("  -w, --warning\t\t\tshows warning dialog \n")
+	fmt.Printf("  -p, --password\t\tshows password dialog \n")
 }
