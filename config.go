ff13fb78989e214f7a0376ac208e0e3fc83a8436
Michal Tvrznik <emporeor@gmail.com>
2021-09-07 13:07:27 +0200

    Used custom module for program arguments handling.

--- config.go
+++ config.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+
+	"github.com/tvrzna/go-utils/args"
 )
 
 type enMsgType int
@@ -24,25 +25,31 @@ type config struct {
 func loadConfig() *config {
 	conf := &config{msgType: warning, title: "asker", message: "Please see help how to use this application."}
 
-	if contains(os.Args, "-h") || contains(os.Args, "--help") {
-		printHelp()
+	if args.ContainsArg(os.Args, "-h", "--help") {
+		fmt.Println("Usage: asker [options]")
+		fmt.Println("Options:")
+		fmt.Printf("  -h, --help\t\t\tprint this help\n")
+		fmt.Printf("  -v, --version\t\t\tprint version\n")
+		fmt.Printf("  -t TEXT, --title TEXT\t\tsets the title\n")
+		fmt.Printf("  -m TEXT, --message TEXT\tsets the message\n")
+		fmt.Printf("  -c, --confirm\t\t\tshows confirm dialog \n")
+		fmt.Printf("  -i, --info\t\t\tshows info dialog \n")
+		fmt.Printf("  -w, --warning\t\t\tshows warning dialog \n")
+		fmt.Printf("  -p, --password\t\tshows password dialog \n")
 		os.Exit(0)
 	}
-	if contains(os.Args, "-v") || contains(os.Args, "--version") {
+
+	if args.ContainsArg(os.Args, "-v", "--version") {
 		fmt.Println(getVersion())
 		os.Exit(0)
 	}
 
-	for i, arg := range os.Args {
+	args.ParseArgs(os.Args, func(arg, nextArg string) {
 		switch arg {
 		case "-t", "--title":
-			nextArg(os.Args, i, func(val string) {
-				conf.title = strings.TrimSpace(val)
-			})
+			conf.title = nextArg
 		case "-m", "--message":
-			nextArg(os.Args, i, func(val string) {
-				conf.message = strings.TrimSpace(val)
-			})
+			conf.message = nextArg
 		case "-c", "--confirm":
 			conf.msgType = confirm
 		case "-w", "--warning":
@@ -52,40 +59,7 @@ func loadConfig() *config {
 		case "-p", "--pasword":
 			conf.msgType = password
 		}
-	}
+	})
 
 	return conf
-}
-
-// Checks, if array contains value
-func contains(array []string, value string) bool {
-	for _, v := range array {
-		if v == value {
-			return true
-		}
-	}
-	return false
-}
-
-// Gets next argument, if available
-func nextArg(args []string, i int, callback func(value string)) {
-	if callback != nil && len(args) > i+1 {
-		val := strings.TrimSpace(args[i+1])
-		if !strings.HasPrefix(val, "-") {
-			callback(args[i+1])
-		}
-	}
-}
-
-func printHelp() {
-	fmt.Println("Usage: asker [options]")
-	fmt.Println("Options:")
-	fmt.Printf("  -h, --help\t\t\tprint this help\n")
-	fmt.Printf("  -v, --version\t\t\tprint version\n")
-	fmt.Printf("  -t TEXT, --title TEXT\t\tsets the title\n")
-	fmt.Printf("  -m TEXT, --message TEXT\tsets the message\n")
-	fmt.Printf("  -c, --confirm\t\t\tshows confirm dialog \n")
-	fmt.Printf("  -i, --info\t\t\tshows info dialog \n")
-	fmt.Printf("  -w, --warning\t\t\tshows warning dialog \n")
-	fmt.Printf("  -p, --password\t\tshows password dialog \n")
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/mattn/go-gtk v0.0.0-20191030024613-af2e013261f5
 	github.com/mattn/go-pointer v0.0.1 // indirect
+	github.com/tvrzna/go-utils/args v0.0.1
 )
